d6c152685cb3d1a46743ad6119758d497a6994f4
Your Name <you@example.com>
2026-01-14 05:27:32 +0000

    m

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+last_digit = -(abs(number) % 10) if (number < 0) else (number % 10)
+if (last_digit > 5):
+    message = "is greater than 5"
+
+elif (last_digit == 0):
+    message = "is 0"
+
+else:
+    message = "is less than 6 and not 0"
+    
+    print(f"Last digit of {number} is {last_digit} and {message}")
