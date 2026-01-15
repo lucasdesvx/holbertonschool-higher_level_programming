9a777d0e795d5542ef9cdfaeacc87c9b1e15e466
Your Name <you@example.com>
2026-01-15 04:46:06 +0000

    m

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python3
 
+def islower(c):
+    """Checks for lowercase character."""
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
