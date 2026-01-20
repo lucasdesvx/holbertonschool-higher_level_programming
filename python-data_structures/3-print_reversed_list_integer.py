b7fe1ac1587b4df44743d9660fc7af8a8689f3f8
Your Name <you@example.com>
2026-01-20 07:33:38 +0000

    m

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """Prints all integers of a list in reverse order."""
+    if not my_list:
+        return
+
+    for i in reversed(my_list):
+        print("{:d}".format(i))
