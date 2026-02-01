bbef33979b216a48a9ecd50c204bca9e01d597ee
Your Name <you@example.com>
2026-02-01 18:07:41 +0000

    m

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Module Rectangle"""
+
+
+class Rectangle:
+    """Classe Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Init"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Aire"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Périmètre"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """String pour print"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(["#" * self.__width for i in range(self.__height)])
+
+    def __repr__(self):
+        """Repr pour eval"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
