1e6eabaf5c1b7b158adb0627d0eb90d9360faf95
Your Name <you@example.com>
2026-02-04 18:26:02 +0000

    m

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+Module qui contient la fonction lookup
+"""
+
+
 def lookup(obj):
+    """
+    Renvoie la liste des attributs et méthodes d'un objet
+    """
     return dir(obj)
