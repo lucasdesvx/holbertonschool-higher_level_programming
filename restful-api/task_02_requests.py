4622aefda3afe43afb37056b0ebc61750605f584
Your Name <you@example.com>
2026-02-22 18:39:45 +0000

    m

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,33 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post.get('title'))
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        data_to_save = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            for post in posts
+        ]
+
+        filename = "posts.csv"
+        fieldnames = ['id', 'title', 'body']
+
+        with open(filename, mode='w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data_to_save)
