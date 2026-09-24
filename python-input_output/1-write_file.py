ef019ef204fb462f746f243c46523a744ae6cd7e
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-09-24 17:06:11 +0400

    m

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Write a string to a UTF-8 text file."""
+"""Module for writing a string to a text file."""
 
 
 def write_file(filename="", text=""):
-    """Write text to filename and return the number of characters written."""
-    with open(filename, "w", encoding="utf-8") as file:
-        return file.write(text)
-
+    """Writes a string to a UTF8 text file and returns the character count."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
