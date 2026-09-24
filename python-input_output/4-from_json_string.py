3c2cfa5654727a8d61cb2ae48f7dc670e0d40ce1
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-09-24 17:31:03 +0400

    m

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Convert a JSON string to a Python object."""
-
+"""Module for converting a JSON string to a Python data structure."""
 import json
 
 
 def from_json_string(my_str):
-    """Return the Python object represented by a JSON string."""
+    """Returns an object (Python data structure) represented by a JSON string."""
     return json.loads(my_str)
-
--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Save a Python object to a file using JSON representation."""
-
+"""Module for writing an object to a text file using JSON representation."""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Write the JSON representation of an object to a file."""
-    with open(filename, "w", encoding="utf-8") as file:
-        json.dump(my_obj, file)
-
+    """Writes an object to a text file using a JSON representation."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
